4462643ad3097ae67f3f8577b2413c763cf4b716
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-17 19:57:34 +0200

    Add navbar configuration

---
+++ TheHistoryOfFormulaOne/src/app/models/ConfigSchema.ts
@@ -0,0 +1,9 @@
+export interface NavItem {
+  id: number
+  title: string;
+  link: string;
+}
+
+export interface ConfigSchema {
+  navItems: NavItem[]
+}
---
+++ TheHistoryOfFormulaOne/src/environments/config.ts
@@ -0,0 +1,22 @@
+import { ConfigSchema } from './../app/models/ConfigSchema';
+
+export const CONFIG: ConfigSchema = {
+  navItems: [
+    { id: 1,
+      title: 'Star Wars',
+      link: '1/'
+    },
+    { id: 2,
+      title: 'Filme',
+      link: '2/'
+    },
+    { id: 3,
+      title: 'Karaktere',
+      link: '3/'
+    },
+    { id: 4,
+      title: 'Planeten',
+      link: '4/',
+    }
+  ]
+}
