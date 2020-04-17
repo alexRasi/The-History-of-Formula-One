aff73433584c3e354c0ed7256e2ab4a542fc448c
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-17 19:57:21 +0200

    Create navbar component

---
+++ TheHistoryOfFormulaOne/src/app/modules/ui/components/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { CONFIG } from './../../../../../environments/config';
+import { Component, OnInit } from '@angular/core';
+import { NavItem } from 'src/app/models/ConfigSchema';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  navItems: NavItem[] = CONFIG.navItems;
+
+  activeNavItemID: number = 2;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isNavItemActive(itemID: number) {
+    return itemID === this.activeNavItemID;
+  }
+
+}
