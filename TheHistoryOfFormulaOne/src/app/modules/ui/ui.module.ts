6aa221fe757c6fbdbdb8d1c74b279291066fd37d
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-18 18:32:41 +0200

    Minor changes

--- TheHistoryOfFormulaOne/src/app/modules/ui/ui.module.ts
+++ TheHistoryOfFormulaOne/src/app/modules/ui/ui.module.ts
@@ -5,9 +5,10 @@ import { NavItemComponent } from './components/nav-item/nav-item.component';
 import { SearchInputComponent } from './components/search-input/search-input.component';
 import { CardDisplayPageComponent } from './components/card-display-page/card-display-page.component';
 import { TitleComponentComponent } from './components/title-component/title-component.component';
+import { DisplayCardComponent } from './components/display-card/display-card.component';
 
 @NgModule({
-  declarations: [NavbarComponent, NavItemComponent, SearchInputComponent, CardDisplayPageComponent, TitleComponentComponent ],
+  declarations: [NavbarComponent, NavItemComponent, SearchInputComponent, CardDisplayPageComponent, TitleComponentComponent, DisplayCardComponent ],
   imports: [
     CommonModule
   ],
