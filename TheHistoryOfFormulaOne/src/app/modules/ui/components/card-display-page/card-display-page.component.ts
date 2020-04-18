7892457eb574edb327222310b88e34a4f3d4bcfd
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-18 15:49:33 +0200

    Create card display page component

---
+++ TheHistoryOfFormulaOne/src/app/modules/ui/components/card-display-page/card-display-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-display-page',
+  templateUrl: './card-display-page.component.html',
+  styleUrls: ['./card-display-page.component.scss']
+})
+export class CardDisplayPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
