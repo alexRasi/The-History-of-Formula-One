27499a2d168982ade5c3e34cccbd7333dee308cf
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-18 15:50:09 +0200

    Add routing

--- TheHistoryOfFormulaOne/src/app/app-routing.module.ts
+++ TheHistoryOfFormulaOne/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'test',
+    loadChildren: () => import('./modules/pages/card-display-page/card-display-page.module').then(m => m.CardDisplayPageModule)
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
