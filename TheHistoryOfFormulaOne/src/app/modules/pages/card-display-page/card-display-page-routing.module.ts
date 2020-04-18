a3ce4c851405e0ea3dfc496cec1d1be98612c102
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-18 15:49:14 +0200

    Create card display page module and routing module

---
+++ TheHistoryOfFormulaOne/src/app/modules/pages/card-display-page/card-display-page-routing.module.ts
@@ -0,0 +1,16 @@
+import { CardDisplayPageComponent } from './../../ui/components/card-display-page/card-display-page.component';
+import { NavbarComponent } from './../../ui/components/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CardDisplayPageComponent
+  }
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CardDisplayPageRoutingModule { }
