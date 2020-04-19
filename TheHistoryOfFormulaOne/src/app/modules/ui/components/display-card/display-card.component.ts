80f6a3bd8f441d914d665802f0e94355315ea194
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-19 15:55:27 +0200

    Bind card data

--- TheHistoryOfFormulaOne/src/app/modules/ui/components/display-card/display-card.component.ts
+++ TheHistoryOfFormulaOne/src/app/modules/ui/components/display-card/display-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-display-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-card.component.scss']
 })
 export class DisplayCardComponent implements OnInit {
+
+  @Input() imageLink: string;
+  @Input() altImageTitle: string;
+  @Input() label: string;
+  @Input() description: string;
+  @Input() moreInfoLink: string;
 
   constructor() { }
 
