84d3e9f9cc7344103e283ced7ade3fcc6144d713
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-19 14:06:48 +0200

    Add dependency injection tokens for every data detching service

--- TheHistoryOfFormulaOne/src/app/app-routing.module.ts
+++ TheHistoryOfFormulaOne/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule, InjectionToken } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SeasonsFetchingService } from './modules/data-fetching/services/data-fetcing-service/data-fetching.service';
+import { SeasonsFetchingService, DriversFetchingService, ConstructorsFetchingService } from './modules/data-fetching/services/data-fetcing-service/data-fetching.service';
 
 const SEASONS_SERVICE_TOKEN = new InjectionToken<string>("SeasonsService");
+const DRIVERS_SERVICE_TOKEN = new InjectionToken<string>("SeasonsService");
+const CONSTRUCTORS_SERVICE_TOKEN = new InjectionToken<string>("SeasonsService");
 
 const routes: Routes = [
   {
@@ -10,6 +12,22 @@ const routes: Routes = [
     data: {
       mainTitle: 'Seasons',
       requiredServiceToken: SEASONS_SERVICE_TOKEN
+    },
+    loadChildren: () => import('./modules/pages/card-display-page/card-display-page.module').then(m => m.CardDisplayPageModule)
+  },
+  {
+    path: 'drivers',
+    data: {
+      mainTitle: 'Drivers',
+      requiredServiceToken: DRIVERS_SERVICE_TOKEN
+    },
+    loadChildren: () => import('./modules/pages/card-display-page/card-display-page.module').then(m => m.CardDisplayPageModule)
+  },
+  {
+    path: 'constructors',
+    data: {
+      mainTitle: 'Constructors',
+      requiredServiceToken: CONSTRUCTORS_SERVICE_TOKEN
     },
     loadChildren: () => import('./modules/pages/card-display-page/card-display-page.module').then(m => m.CardDisplayPageModule)
   }
@@ -21,6 +39,14 @@ const routes: Routes = [
     {
       provide: SEASONS_SERVICE_TOKEN,
       useClass: SeasonsFetchingService
+    },
+    {
+      provide: DRIVERS_SERVICE_TOKEN,
+      useClass: DriversFetchingService
+    },
+    {
+      provide: CONSTRUCTORS_SERVICE_TOKEN,
+      useClass: ConstructorsFetchingService
     }
   ]
 })
