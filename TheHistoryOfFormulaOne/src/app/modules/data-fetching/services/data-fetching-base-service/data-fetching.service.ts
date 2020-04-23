a8f9791f16ebb2b1901a1db93371c3fb7c89bd46
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-23 13:54:35 +0200

    Make data fetching base service type generic and add a Cache member

--- TheHistoryOfFormulaOne/src/app/modules/data-fetching/services/data-fetching-base-service/data-fetching.service.ts
+++ TheHistoryOfFormulaOne/src/app/modules/data-fetching/services/data-fetching-base-service/data-fetching.service.ts
@@ -1,8 +1,8 @@
-import { ItemGenericDetail } from './../../../../models/ItemGenericDetail';
 import { Observable } from 'rxjs';
-import { CardGenericData } from './../../../../models/CardGenericData';
-import { CardDisplayPageGenericData } from 'src/app/models/CardDisplayPageGenericData';
-export abstract class DataFetchingService {
-  abstract getData(parameter?: any, limit?: number, offset?: number): any
-  abstract getTransformedData(parameter?: any, limit?: number, offset?: number): any
+
+export abstract class DataFetchingService<CacheArrayType> {
+  CACHE: CacheArrayType[] = [];
+
+  abstract getData(parameter?: any, limit?: number, offset?: number): Observable<any>;
+  abstract getTransformedData(parameter?: any, limit?: number, offset?: number): Observable<any>;
 }
