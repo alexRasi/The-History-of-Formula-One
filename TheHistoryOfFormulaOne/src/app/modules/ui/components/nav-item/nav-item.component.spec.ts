9364df4c6179f887cb4f2e83e4751cffe408ce7a
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-25 01:40:43 +0200

    Add nav item test

--- TheHistoryOfFormulaOne/src/app/modules/ui/components/nav-item/nav-item.component.spec.ts
+++ TheHistoryOfFormulaOne/src/app/modules/ui/components/nav-item/nav-item.component.spec.ts
@@ -1,3 +1,4 @@
+import { By } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavItemComponent } from './nav-item.component';
@@ -20,10 +21,31 @@ describe('NavItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(NavItemComponent);
     component = fixture.componentInstance;
+
+    component.title = 'Test title';
+    component.link = 'Test link';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a title', () => {
+    expect(component.title).toBeTruthy();
+  });
+
+  it('should have a link', () => {
+    expect(component.link).toBeTruthy();
+  });
+
+  it('should display a title', () => {
+    const content = fixture.debugElement.nativeElement.querySelector('a');
+    expect(content.innerHTML).toContain('Test title');
+  });
+
+  it('should have a link', () => {
+    const content = fixture.debugElement.nativeElement.querySelector('a');
+    expect(content.href).toBeTruthy();
   });
 });
