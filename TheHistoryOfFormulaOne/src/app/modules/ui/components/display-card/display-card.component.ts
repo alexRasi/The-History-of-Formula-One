9605201d52f5ad9f630791952aa6314e6c7292b4
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-25 01:28:10 +0200

    Minor fixes

--- TheHistoryOfFormulaOne/src/app/modules/ui/components/display-card/display-card.component.ts
+++ TheHistoryOfFormulaOne/src/app/modules/ui/components/display-card/display-card.component.ts
@@ -17,7 +17,10 @@ export class DisplayCardComponent {
     var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
     var regex = new RegExp(expression);
 
-    return str.match(regex);
+    if(this.description) {
+      return this.description.match(regex);
+    } else {
+      return false;
+    }
   }
-
 }
