9c6cd755a124f90ae6a373fe5c2298550caab50b
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-25 19:10:26 +0200

    Add app component's test

--- TheHistoryOfFormulaOne/src/app/app.component.spec.ts
+++ TheHistoryOfFormulaOne/src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
+import { SpinLoaderComponent } from './modules/ui/components/spin-loader/spin-loader.component';
+import { FooterComponent } from './modules/ui/components/footer/footer.component';
+import { By } from '@angular/platform-browser';
 import { UiModule } from './modules/ui/ui.module';
 import { NavbarComponent } from './modules/ui/components/navbar/navbar.component';
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
+
+let fixture: ComponentFixture<AppComponent>
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -15,11 +21,28 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should have a navbar', () => {
+    expect(fixture.debugElement.query(By.directive(NavbarComponent))).toBeTruthy();
+  });
+
+  it('should have a router outlet', () => {
+    expect(fixture.debugElement.query(By.directive(RouterOutlet))).toBeTruthy();
+  });
+
+  it('should have a footer', () => {
+    expect(fixture.debugElement.query(By.directive(FooterComponent))).toBeTruthy();
+  });
+
+  it('should have a loading spinner component', () => {
+    expect(fixture.debugElement.query(By.directive(SpinLoaderComponent))).toBeTruthy();
   });
 });
