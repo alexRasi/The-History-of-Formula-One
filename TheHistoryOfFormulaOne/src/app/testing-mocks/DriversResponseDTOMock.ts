49ecabd6745f0c542428843af484ba0275d2a04f
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-25 21:48:30 +0200

    Create response and transformed mocks

---
+++ TheHistoryOfFormulaOne/src/app/testing-mocks/DriversResponseDTOMock.ts
@@ -0,0 +1,68 @@
+export const DriversResponseDTOMock = {"MRData":{"xmlns":"http:\/\/ergast.com\/mrd\/1.4","series":"f1","url":"http://ergast.com/api/f1/drivers.json","limit":"12","offset":"0","total":"848","DriverTable":{"Drivers":[{"driverId":"abate","url":"http:\/\/en.wikipedia.org\/wiki\/Carlo_Mario_Abate","givenName":"Carlo","familyName":"Abate","dateOfBirth":"1932-07-10","nationality":"Italian"},{"driverId":"abecassis","url":"http:\/\/en.wikipedia.org\/wiki\/George_Abecassis","givenName":"George","familyName":"Abecassis","dateOfBirth":"1913-03-21","nationality":"British"},{"driverId":"acheson","url":"http:\/\/en.wikipedia.org\/wiki\/Kenny_Acheson","givenName":"Kenny","familyName":"Acheson","dateOfBirth":"1957-11-27","nationality":"British"},{"driverId":"adams","url":"http:\/\/en.wikipedia.org\/wiki\/Philippe_Adams","givenName":"Philippe","familyName":"Adams","dateOfBirth":"1969-11-19","nationality":"Belgian"},{"driverId":"ader","url":"http:\/\/en.wikipedia.org\/wiki\/Walt_Ader","givenName":"Walt","familyName":"Ader","dateOfBirth":"1913-12-15","nationality":"American"},{"driverId":"adolff","url":"http:\/\/en.wikipedia.org\/wiki\/Kurt_Adolff","givenName":"Kurt","familyName":"Adolff","dateOfBirth":"1921-11-05","nationality":"German"},{"driverId":"agabashian","url":"http:\/\/en.wikipedia.org\/wiki\/Fred_Agabashian","givenName":"Fred","familyName":"Agabashian","dateOfBirth":"1913-08-21","nationality":"American"},{"driverId":"ahrens","url":"http:\/\/en.wikipedia.org\/wiki\/Kurt_Ahrens,_Jr.","givenName":"Kurt","familyName":"Ahrens","dateOfBirth":"1940-04-19","nationality":"German"},{"driverId":"albers","code":"ALB","url":"http:\/\/en.wikipedia.org\/wiki\/Christijan_Albers","givenName":"Christijan","familyName":"Albers","dateOfBirth":"1979-04-16","nationality":"Dutch"},{"driverId":"albon","permanentNumber":"23","code":"ALB","url":"http:\/\/en.wikipedia.org\/wiki\/Alexander_Albon","givenName":"Alexander","familyName":"Albon","dateOfBirth":"1996-03-23","nationality":"Thai"},{"driverId":"alboreto","url":"http:\/\/en.wikipedia.org\/wiki\/Michele_Alboreto","givenName":"Michele","familyName":"Alboreto","dateOfBirth":"1956-12-23","nationality":"Italian"},{"driverId":"alesi","url":"http:\/\/en.wikipedia.org\/wiki\/Jean_Alesi","givenName":"Jean","familyName":"Alesi","dateOfBirth":"1964-06-11","nationality":"French"}]}}}
+
+export const TransformedDriversResponseMock = {
+  "cards": [
+    {
+      "label": "Abate Carlo",
+      "description": "http://en.wikipedia.org/wiki/Carlo_Mario_Abate",
+      "moreInfoLink": "/drivers/abate"
+    },
+    {
+      "label": "Abecassis George",
+      "description": "http://en.wikipedia.org/wiki/George_Abecassis",
+      "moreInfoLink": "/drivers/abecassis"
+    },
+    {
+      "label": "Acheson Kenny",
+      "description": "http://en.wikipedia.org/wiki/Kenny_Acheson",
+      "moreInfoLink": "/drivers/acheson"
+    },
+    {
+      "label": "Adams Philippe",
+      "description": "http://en.wikipedia.org/wiki/Philippe_Adams",
+      "moreInfoLink": "/drivers/adams"
+    },
+    {
+      "label": "Ader Walt",
+      "description": "http://en.wikipedia.org/wiki/Walt_Ader",
+      "moreInfoLink": "/drivers/ader"
+    },
+    {
+      "label": "Adolff Kurt",
+      "description": "http://en.wikipedia.org/wiki/Kurt_Adolff",
+      "moreInfoLink": "/drivers/adolff"
+    },
+    {
+      "label": "Agabashian Fred",
+      "description": "http://en.wikipedia.org/wiki/Fred_Agabashian",
+      "moreInfoLink": "/drivers/agabashian"
+    },
+    {
+      "label": "Ahrens Kurt",
+      "description": "http://en.wikipedia.org/wiki/Kurt_Ahrens,_Jr.",
+      "moreInfoLink": "/drivers/ahrens"
+    },
+    {
+      "label": "Albers Christijan",
+      "description": "http://en.wikipedia.org/wiki/Christijan_Albers",
+      "moreInfoLink": "/drivers/albers"
+    },
+    {
+      "label": "Albon Alexander",
+      "description": "http://en.wikipedia.org/wiki/Alexander_Albon",
+      "moreInfoLink": "/drivers/albon"
+    },
+    {
+      "label": "Alboreto Michele",
+      "description": "http://en.wikipedia.org/wiki/Michele_Alboreto",
+      "moreInfoLink": "/drivers/alboreto"
+    },
+    {
+      "label": "Alesi Jean",
+      "description": "http://en.wikipedia.org/wiki/Jean_Alesi",
+      "moreInfoLink": "/drivers/alesi"
+    }
+  ],
+  "title": "Drivers",
+  "totalData": 848
+};
---
+++ TheHistoryOfFormulaOne/src/app/testing-mocks/SeasonsResponseDTOMock.ts
@@ -0,0 +1,69 @@
+export const SeasonsResponseDTOMock = { "MRData": { "xmlns": "http:\/\/ergast.com\/mrd\/1.4", "series": "f1", "url": "http://ergast.com/api/f1/seasons.json", "limit": "12", "offset": "0", "total": "71", "SeasonTable": { "Seasons": [{ "season": "1950", "url": "https:\/\/en.wikipedia.org\/wiki\/1950_Formula_One_season" }, { "season": "1951", "url": "https:\/\/en.wikipedia.org\/wiki\/1951_Formula_One_season" }, { "season": "1952", "url": "https:\/\/en.wikipedia.org\/wiki\/1952_Formula_One_season" }, { "season": "1953", "url": "https:\/\/en.wikipedia.org\/wiki\/1953_Formula_One_season" }, { "season": "1954", "url": "https:\/\/en.wikipedia.org\/wiki\/1954_Formula_One_season" }, { "season": "1955", "url": "https:\/\/en.wikipedia.org\/wiki\/1955_Formula_One_season" }, { "season": "1956", "url": "https:\/\/en.wikipedia.org\/wiki\/1956_Formula_One_season" }, { "season": "1957", "url": "https:\/\/en.wikipedia.org\/wiki\/1957_Formula_One_season" }, { "season": "1958", "url": "https:\/\/en.wikipedia.org\/wiki\/1958_Formula_One_season" }, { "season": "1959", "url": "https:\/\/en.wikipedia.org\/wiki\/1959_Formula_One_season" }, { "season": "1960", "url": "https:\/\/en.wikipedia.org\/wiki\/1960_Formula_One_season" }, { "season": "1961", "url": "https:\/\/en.wikipedia.org\/wiki\/1961_Formula_One_season" }] } } }
+
+
+export const TransformedSeasonsResponseMock = {
+  "cards": [
+    {
+      "label": "1950",
+      "description": "https://en.wikipedia.org/wiki/1950_Formula_One_season",
+      "moreInfoLink": "season-standings/1950"
+    },
+    {
+      "label": "1951",
+      "description": "https://en.wikipedia.org/wiki/1951_Formula_One_season",
+      "moreInfoLink": "season-standings/1951"
+    },
+    {
+      "label": "1952",
+      "description": "https://en.wikipedia.org/wiki/1952_Formula_One_season",
+      "moreInfoLink": "season-standings/1952"
+    },
+    {
+      "label": "1953",
+      "description": "https://en.wikipedia.org/wiki/1953_Formula_One_season",
+      "moreInfoLink": "season-standings/1953"
+    },
+    {
+      "label": "1954",
+      "description": "https://en.wikipedia.org/wiki/1954_Formula_One_season",
+      "moreInfoLink": "season-standings/1954"
+    },
+    {
+      "label": "1955",
+      "description": "https://en.wikipedia.org/wiki/1955_Formula_One_season",
+      "moreInfoLink": "season-standings/1955"
+    },
+    {
+      "label": "1956",
+      "description": "https://en.wikipedia.org/wiki/1956_Formula_One_season",
+      "moreInfoLink": "season-standings/1956"
+    },
+    {
+      "label": "1957",
+      "description": "https://en.wikipedia.org/wiki/1957_Formula_One_season",
+      "moreInfoLink": "season-standings/1957"
+    },
+    {
+      "label": "1958",
+      "description": "https://en.wikipedia.org/wiki/1958_Formula_One_season",
+      "moreInfoLink": "season-standings/1958"
+    },
+    {
+      "label": "1959",
+      "description": "https://en.wikipedia.org/wiki/1959_Formula_One_season",
+      "moreInfoLink": "season-standings/1959"
+    },
+    {
+      "label": "1960",
+      "description": "https://en.wikipedia.org/wiki/1960_Formula_One_season",
+      "moreInfoLink": "season-standings/1960"
+    },
+    {
+      "label": "1961",
+      "description": "https://en.wikipedia.org/wiki/1961_Formula_One_season",
+      "moreInfoLink": "season-standings/1961"
+    }
+  ],
+  "title": "Seasons",
+  "totalData": 71
+};
