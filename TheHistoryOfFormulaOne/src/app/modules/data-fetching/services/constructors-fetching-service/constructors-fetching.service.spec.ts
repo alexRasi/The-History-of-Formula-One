cc7c0f01242c6ad04e7a6b5faaa6a88acee3a418
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-25 21:42:24 +0200

    Implement constructors fetching service test

--- TheHistoryOfFormulaOne/src/app/modules/data-fetching/services/constructors-fetching-service/constructors-fetching.service.spec.ts
+++ TheHistoryOfFormulaOne/src/app/modules/data-fetching/services/constructors-fetching-service/constructors-fetching.service.spec.ts
@@ -1,7 +1,10 @@
+import { ConstructorsResponseDTOMock, TransformedConstructorsResponseMock } from './../../../../testing-mocks/ConstructorsResponseDTOMock';
+import { ConstructorsResponseDTO } from './../../../../models/dtos/ConstructorsResponseDTO';
 import { TestBed } from '@angular/core/testing';
 
 import { ConstructorsFetchingService } from './constructors-fetching.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 
 describe('ConstructorsFetchingService', () => {
@@ -13,5 +16,15 @@ describe('ConstructorsFetchingService', () => {
   it('should be created', () => {
     const service: ConstructorsFetchingService = TestBed.get(ConstructorsFetchingService);
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch a ConstructorItemResponseDTO array and return it transformed to ItemDisplayPageGenericData', async () => {
+    const service: ConstructorsFetchingService = TestBed.get(ConstructorsFetchingService)
+    spyOn(service,'getData').and.returnValue(of(ConstructorsResponseDTOMock));
+
+    let transformedData;
+    service.getTransformedData(undefined,undefined,undefined).subscribe( data => transformedData = data);
+
+    expect(transformedData).toEqual(TransformedConstructorsResponseMock);
   });
 });
