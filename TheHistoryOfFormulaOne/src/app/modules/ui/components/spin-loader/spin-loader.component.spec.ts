e8235d3b4d31be044dc1b474fddbdd1993f6b4c1
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-26 02:57:09 +0200

    Implement spin loader component test

--- TheHistoryOfFormulaOne/src/app/modules/ui/components/spin-loader/spin-loader.component.spec.ts
+++ TheHistoryOfFormulaOne/src/app/modules/ui/components/spin-loader/spin-loader.component.spec.ts
@@ -1,16 +1,19 @@
+import { LoadingSpinnerService } from './../../services/loading-spinner-service/loading-spinner.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SpinLoaderComponent } from './spin-loader.component';
+import { DebugElement } from '@angular/core';
 
 describe('SpinLoaderComponent', () => {
   let component: SpinLoaderComponent;
   let fixture: ComponentFixture<SpinLoaderComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SpinLoaderComponent ]
+      declarations: [SpinLoaderComponent],
+      providers: [LoadingSpinnerService]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -22,4 +25,29 @@ describe('SpinLoaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('If spinloader service showSpinner() is called', () => {
+    beforeEach(async(() => {
+      component.loadingSpinnerService.showSpinner();
+      fixture.detectChanges();
+    }));
+
+    it('loader should be rendered ', async () => {
+      expect(fixture.debugElement.nativeElement.querySelector('.lds-ripple')).toBeTruthy();
+    });
+
+  });
+
+  describe('If spinloader service hideSpinner() is called', () => {
+    beforeEach(async(() => {
+      component.loadingSpinnerService.hideSpinner();
+      fixture.detectChanges();
+    }));
+
+    it('loader should not be rendered ', async () => {
+      expect(fixture.debugElement.nativeElement.querySelector('.lds-ripple')).toBeFalsy();
+    });
+
+  });
+
 });
