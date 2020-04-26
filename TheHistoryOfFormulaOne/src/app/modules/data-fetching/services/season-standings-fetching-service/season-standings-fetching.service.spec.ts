2cacf0df4b37ca44f5cf22c012763bb104e961e1
a.rasidakis <a.rasidakis@interfacema.de>
2020-04-26 02:09:01 +0200

    Add season standings fetching service testing

--- TheHistoryOfFormulaOne/src/app/modules/data-fetching/services/season-standings-fetching-service/season-standings-fetching.service.spec.ts
+++ TheHistoryOfFormulaOne/src/app/modules/data-fetching/services/season-standings-fetching-service/season-standings-fetching.service.spec.ts
@@ -1,7 +1,10 @@
+import { TransformedSeasonStandingsResponseMock, SeasonStandingsResponseDTOMock } from './../../../../testing-mocks/SeasonStandingsResponseDTOMock';
+import { SeasonStandingsResponseDTO } from './../../../../models/dtos/SeasonStandingsResponseDTO';
 import { TestBed } from '@angular/core/testing';
 
 import { SeasonStandingsFetchingService } from './season-standings-fetching.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 describe('SeasonStandingsFetchingService', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -12,5 +15,15 @@ describe('SeasonStandingsFetchingService', () => {
   it('should be created', () => {
     const service: SeasonStandingsFetchingService = TestBed.get(SeasonStandingsFetchingService);
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch a SeasonStandingDTO object and return it transformed to CardDisplayPageGenericData', async () => {
+    const service: SeasonStandingsFetchingService = TestBed.get(SeasonStandingsFetchingService)
+    spyOn(service, 'getData').and.returnValue(of(SeasonStandingsResponseDTOMock));
+
+    let transformedData;
+    service.getTransformedData(undefined, undefined, undefined).subscribe(data => transformedData = data);
+
+    expect(transformedData).toEqual(TransformedSeasonStandingsResponseMock);
   });
 });
